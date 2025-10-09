a86c05738847466840e09c4ee98124e515879635
marshallhenhawke <marshall@allcareit.com>
2025-10-09 12:13:06 -0400

    build: include dist for Nuxt 4 compatibility

---
+++ dist/module.d.mts
@@ -0,0 +1,8 @@
+interface ModuleOptions {
+    mode: 'full' | 'slim' | 'basic' | 'custom';
+    lazy: boolean;
+}
+declare const _default: any;
+
+export { _default as default };
+export type { ModuleOptions };
---
+++ dist/module.mjs
@@ -0,0 +1,35 @@
+import { defineNuxtModule, createResolver, addPlugin, addComponent } from '@nuxt/kit';
+import defu from 'defu';
+
+const module = defineNuxtModule({
+  meta: {
+    name: "nuxt-particles",
+    configKey: "particles",
+    compatibility: {
+      nuxt: "^3.7.0"
+    }
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {
+    mode: "full",
+    lazy: true
+  },
+  setup(options, nuxt) {
+    const resolver = createResolver(import.meta.url);
+    if (!options.lazy && options.mode !== "custom") {
+      addPlugin(resolver.resolve("./runtime/plugins/particle-loader.client"));
+    }
+    nuxt.options.runtimeConfig.public = defu(nuxt.options.runtimeConfig.public || {}, {
+      particles: {
+        mode: options.mode,
+        lazy: options.lazy
+      }
+    });
+    addComponent({
+      name: "NuxtParticles",
+      filePath: resolver.resolve("./runtime/components/NuxtParticles.vue")
+    });
+  }
+});
+
+export { module as default };
